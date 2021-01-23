7146d9adafa91883b64bdd0b5719212b08f6de27
VadimHome <vadimtsh@gmail.com>
2021-01-23 19:13:06 +0300

    add test_Lesson_2

---
+++ src/test/java/Lesson_2/TestLesson2.java
@@ -0,0 +1,30 @@
+package Lesson_2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLesson2 {
+    @Test(expected = MyArraySizeException.class)
+    public void testMyArraySizeExeption() {
+        int res = Lesson_2.demoException(new String[][]{});
+        Assert.assertEquals(136, res);
+    }
+
+    @Test(expected = MyArrayDataException.class)
+    public void MyArrayDataException() {
+        int res = Lesson_2.demoException(new String[][]{{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "и", "16"}});
+        Assert.assertEquals(-1, res);
+    }
+
+    @Test
+    public void testOk() {
+        int res = Lesson_2.demoException(new String[][]{{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}});
+        Assert.assertEquals(136, res);
+    }
+}
